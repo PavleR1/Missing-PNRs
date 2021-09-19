d8a201e754770918f8a8e3e23c0694a1f6cb78b3
HP 830 G5 <pavlerakovic1@gmail.com>
2021-09-19 14:12:00 +0200

    PNR.db  added, README.md writen, database  handling implemented

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Missing-PNRs
+
+Description: Script main.py is made to find all missing PNRs between given two. Missing PNRs are stored in Database called PNR.db same as two given PNRs while client get a number of missing PNRs.
+
+# Requirements
+
+ -Python 3.x
+ -sqlite3 (built in into python)
+ -string  (built in into python)
+ -unittest (built in into python)
+ (requirements.txt file was made by "pip freeze > requirements.txt" command)
+
+# How to use
+
+  Script main.py can be used in two ways :
+
+  1) It can be run directly which provides interaction with client through the console/terminal 
+  2) Script can be importet in another script and all it functionality can be used 
+
+  Script contain class Detector with attributes/metods:
+  	1. d_s2n - dict that contain symbols(0-9, A-Z) for keys and numbers(0-35) as values
+  	2. d_n2s - dict that contain numbers(0-35) for keys and symbols(0-9, A-Z) as values
+  	3. base - nubmber used to convert PNRs to decimal format
+  	4. conn - connection to Database
+  	5. c - cursor that execute queries 
+
+  	6. __init__ - constructor seting up attributes
+  	7. uspostavi_konekciju - function that provide connection to Database
+  	8. save_to_db(fist, second, missing) - function that saves first given PNR with isMissing = 0 , second given PNR with isMissing = 0 and each missing PNR from list missing with isMissing = 1
+  	9. close_connection - closing connection with Database
+  	10. check_order(x, y) - checking if y is greater than x if it is returned value is x,y , if x is greater than returned values is y,x. ValueError will be raised if x and y are the equal
+  	11. convert(x) - function convert integer number into PNR or PNR into integer number.ValueError is raised if PNR is in wrong format(less/more than 6 symbols , symbols are not in d_s2n.keys() or symbol equals 0) or if given value x is of a diferent data type except string or integer
+  	12. find_missing_PNR (x,y) - function whit a job to count and return how many missing PNRs are between 2 given. After counting, finds the missing PNRs and stores tham in Database. ValueError is raised when number of missing PNRs is greater than 1000
+
+# Testing
+
+ The unittest package was used for testing purposes.Testing was pperformed on convert,check_order and find_missing_PNR.
+ The behavior when passing valid values was tested, as well as the behavior when passing values that should raise ValueError. 
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import string
 import sqlite3
-import sys
 
 class Detector():
 	_d_s2n = {}		# dict symbol : number
 	_d_n2s = {}		# dict number: symbol
 	_base =  0      # base used to convert PNR to decimal 
-	_conn = None
+	_conn = None	# connection to database
+	_c = None		# cursor
 
 	def __init__(self):
 		self._d_s2n = {string.ascii_uppercase[i]:range(10,26+10)[i] for i in range(26)}
 		self._d_s2n.update({str(i):i for i in range(0,10)})		# dict symbol : number
 		self._d_n2s = dict(map(reversed, self._d_s2n.items()))	# dict number: symbol
 		self._base =  35
-		self._conn = sqlite3.connect('PNR.db')
+		self.uspostavi_konekciju()
 
 	def find_missing_PNR(self, x, y):
 
@@ -27,6 +27,7 @@ class Detector():
 				raise ValueError('There are more than 1000 missing PNRs\n')
 			
 			list_of_missing_pnrs = [self.convert(i) for i in range(a+1,b)]
+			self.save_to_db(x,y,list_of_missing_pnrs)
 			
 			return n_missing
 
@@ -36,7 +37,8 @@ class Detector():
 		if type(x) == str and len(x)==6:
 			p = 0
 			for char in x:
-				if char == '0' or not(char in self._d_s2n.keys()) or char.islower(): raise error
+				if char == '0' or not(char in self._d_s2n.keys()) or char.islower(): 
+					raise error
 				p *= self._base
 				p += self._d_s2n[char]	
 			return p
@@ -55,14 +57,42 @@ class Detector():
 			raise ValueError("Givend PNRs are the same")
 		return y, x
 
+	def uspostavi_konekciju(self):
+		self._conn = sqlite3.connect('PNR.db')
+		self._c = self._conn.cursor()
+		try:
+			self._c.execute("CREATE TABLE pnrs (pnrID integer PRIMARY KEY,PNR text,isMissing integer)")
+			self._conn.commit()
+		except :
+			pass
+
+	def save_to_db(self, first, second, missing):
+		try:
+			self._c.execute("INSERT INTO pnrs (PNR,isMissing) VALUES (?,?)",(first,0))
+			self._c.execute("INSERT INTO pnrs (PNR,isMissing) VALUES (?,?)",(second,0))
+			for m in missing:
+				self._c.execute("INSERT INTO pnrs (PNR,isMissing) VALUES (?,?)",(m,1))
+			self._conn.commit()
+		except Exception as e:
+			print(e)
+			self._conn.rollback()
+	
+	def close_conection(self):
+		self._conn.close()
+
+
 if __name__ == '__main__':
 	d = Detector()
 	while(True):
 		print("Type exit to stop runing program\n")
 		a = input("First PNR : ") 
-		if a.lower() == "exit" : break
+		if a.lower() == "exit" : 
+			d.close_conection()
+			break
 		b = input("Second PNR : ")
-		if b.lower() == "exit" : break
+		if b.lower() == "exit" : 
+			d.close_conection()
+			break
 		try:
 			print(d.find_missing_PNR(a,b))
 		except Exception as e:
